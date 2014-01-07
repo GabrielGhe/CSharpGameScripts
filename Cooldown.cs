932d8e974227eb19b8786e3af4f9792251ba108f
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-01-07 13:25:43 -0500

    Some minor scripts

---
+++ Cooldown.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cooldown : MonoBehaviour {
+
+	private bool onCooldown = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if a gets pressed, key was used... can also be
+		//called in as method
+		if(Input.GetKeyDown("a")){
+			onCooldown = true;
+			this.wait(2); // waits 2 seconds
+		}
+	}
+
+	/// <summary>
+	/// Wait the specified time.
+	/// </summary>
+	/// <param name="time">Time.</param>
+	private IEnumerator wait(int time){
+		yield return new WaitForSeconds (time * 1.0f);
+		onCooldown = false;
+	}
+}
---
+++ Pause.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	private bool paused = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// To pause
+		if (Input.GetKeyDown ("p") && !paused) {
+			paused = true;
+			Time.timeScale = 0;
+		}
+
+		// To unpause
+		if (Input.GetKeyDown ("p") && paused) {
+			paused = false;
+			Time.timeScale = 1;
+		}
+	}
+}
