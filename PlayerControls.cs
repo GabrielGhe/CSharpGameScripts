f8bdc8632b8f440c21cbf406eb71f7534aba8e3b
Gabriel Gheorghian <gabriel.gheorghian@gmail.com>
2014-01-07 21:58:10 -0500

    added player script for pong

---
+++ PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour {
+
+	public KeyCode moveUp;
+	public KeyCode moveDown;
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(moveUp)) {
+			//pressing the moveUp key
+			rigidbody2D.velocity = new Vector3(0,speed,0);
+			
+		} else if (Input.GetKey(moveDown)) {
+			//pressing the moveDown key
+			rigidbody2D.velocity = new Vector3(0,speed * -1,0);
+
+		} else {
+			//pressing another key
+			rigidbody2D.velocity = new Vector3(0,0,0);
+		}
+	
+	}
+}
